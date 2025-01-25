b808c79e0e128f2c9f3304d7e94b46870afe7449
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-01-25 16:02:23 +0100

    fix 4

--- src/stream-ui/js/helper.js
+++ src/stream-ui/js/helper.js
@@ -1,10 +1,10 @@
 class Helper {
     static durationToSwimTimeString(duration) {
         if (!duration) return "-";
-        let d = new Date((duration / 1000) / 1000)
+        let d = new Date(Math.floor((duration / 1000) / 1000))
         let minutes = "0" + d.getMinutes()
         let seconds = "0" + d.getSeconds()
-        let millis = "0" + (d.getMilliseconds() / 10)
+        let millis = "0" + Math.floor(d.getMilliseconds() / 10)
         return minutes.substr(-2) + ":" + seconds.substr(-2) + "," + millis.substr(-2)
     }
 
